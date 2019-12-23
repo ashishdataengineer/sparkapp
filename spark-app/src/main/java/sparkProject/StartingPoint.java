6062e602a2503c97a9ba759a811ab0e115523b36
Ashish <ashishetllife@gmail.com>
2019-12-23 20:35:32 +0530

    Edited

--- spark-app/src/main/java/sparkProject/StartingPoint.java
+++ spark-app/src/main/java/sparkProject/StartingPoint.java
@@ -1,14 +1,11 @@
 package sparkProject;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 import javax.xml.transform.TransformerException;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Row;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
